b8a401ac153032445e2fe5f6f05b4c1c8d5dcd4a
icyfig <icyfig@gmail.com>
2023-01-22 17:36:23 +0530

    Home Page Created

---
+++ pages/contact.js
@@ -0,0 +1,14 @@
+
+import Layout from "@/components/layout"
+import styles from '@/styles/HomePage.module.css'
+
+export default function Contact() {
+    return (
+        <Layout>
+            <div className={styles.contact}>
+                <h1>Contact</h1>
+                <p>+91 2497249242424</p>
+            </div>
+        </Layout>
+    )
+}
